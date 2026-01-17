77e38803b70c10faf2ec1e1066d98214078647e9
nathan.morris <nmorris@alberton.k12.mt.us>
2026-01-16 22:54:30 -0700

    Need to fix ButteTimeAuto to have self-contained mecanum drive logic to be able to run even if pedropathing isn't working

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/InDevelopment/ButteTimeAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/InDevelopment/ButteTimeAuto.java
@@ -1,56 +1,44 @@
-//package org.firstinspires.ftc.teamcode.InDevelopment;
-//
-//import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-//import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-//import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-//import com.qualcomm.robotcore.util.ElapsedTime;
-//
-//import org.firstinspires.ftc.teamcode.RobotHardware.IntakeHardware;
-//import org.firstinspires.ftc.teamcode.RobotHardware.LauncherHardware;
-//import org.firstinspires.ftc.teamcode.RobotHardware.TransferHardware;
-//
-//@Disabled
-//@Autonomous(name="ButteTimeAuto", group="01 Butte", preselectTeleOp="Butte2Controller")
-//public class ButteTimeAuto extends OpMode{
-//    LauncherHardware launcherHardware = new LauncherHardware();
-//    IntakeHardware intakeHardware = new IntakeHardware();
-//    //MecanumHardware mecanumHardware = new MecanumHardware();
-//    TransferHardware transferHardware = new TransferHardware();
-//    private ElapsedTime runtime = new ElapsedTime();
-//
-//    @Override
-//    public void init(){
-//        launcherHardware.init(hardwareMap);
-//        intakeHardware.init(hardwareMap);
-//        mecanumHardware.init(hardwareMap);
-//        transferHardware.init(hardwareMap);
-//        telemetry.addData("Status", "Initialized");
-//        telemetry.update();
-//    }
-//
-//    @Override
-//    public void start() {
-//        runtime.reset();
-//    }
-//
-//    @Override
-//    public void loop() {
-//        if (runtime.seconds() < 5){
-//            mecanumHardware.drive(.5, 0, 0);
-//        }
-//        telemetry.addData("Status", "Autonomous Running");
-//        telemetry.update();
-//    }
-//
-//    @Override
-//    public void stop() {
-//        telemetry.addData("Status", "Autonomous Finished");
-//        telemetry.update();
-//        mecanumHardware.stop();
-//        transferHardware.stop();
-//        intakeHardware.stop();
-//        launcherHardware.stop();
-//
-//    }
-//
-//}
+package org.firstinspires.ftc.teamcode.InDevelopment;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Disabled
+@Autonomous(name="ButteTimeAuto", group="01 Butte", preselectTeleOp="Butte2Controller")
+public class ButteTimeAuto extends OpMode{
+
+    private ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+    public void init(){
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+    }
+
+    @Override
+    public void start() {
+        runtime.reset();
+    }
+
+    @Override
+    public void loop() {
+        if (runtime.seconds() < 5){
+            mecanumHardware.drive(.5, 0, 0);
+        }
+        telemetry.addData("Status", "Autonomous Running");
+        telemetry.update();
+    }
+
+    @Override
+    public void stop() {
+        telemetry.addData("Status", "Autonomous Finished");
+        telemetry.update();
+        mecanumHardware.stop();
+
+
+    }
+
+}
