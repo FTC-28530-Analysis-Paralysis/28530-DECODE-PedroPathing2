a3621dee5b8e545da1b8fdaa1159c8949a2a86fe
mahlo <mahlon.philip.manson@gmail.com>
2026-01-16 23:38:54 -0700

    made drive by time

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/InDevelopment/ButteTimeAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/InDevelopment/ButteTimeAuto.java
@@ -3,16 +3,37 @@ package org.firstinspires.ftc.teamcode.InDevelopment;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@Disabled
+//@Disabled
 @Autonomous(name="ButteTimeAuto", group="01 Butte", preselectTeleOp="Butte2Controller")
 public class ButteTimeAuto extends OpMode{
 
+    DcMotor frontLeftDrive;
+    DcMotor frontRightDrive;
+    DcMotor backLeftDrive;
+    DcMotor backRightDrive;
     private ElapsedTime runtime = new ElapsedTime();
 
     @Override
     public void init(){
+        frontLeftDrive = hardwareMap.get(DcMotor.class, "left_front_drive");
+        frontRightDrive = hardwareMap.get(DcMotor.class, "right_front_drive");
+        backLeftDrive = hardwareMap.get(DcMotor.class, "left_back_drive");
+        backRightDrive = hardwareMap.get(DcMotor.class, "right_back_drive");
+
+        // We set the left motors in reverse which is needed for drive trains where the left
+        // motors are opposite to the right ones.
+        backLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+        frontLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+
+        // This uses RUN_USING_ENCODER to be more accurate.   If you don't have the encoder
+        // wires, you should remove these
+        frontLeftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
@@ -25,20 +46,43 @@ public class ButteTimeAuto extends OpMode{
 
     @Override
     public void loop() {
-        if (runtime.seconds() < 5){
-            //mecanumHardware.drive(.5, 0, 0);
+        if (runtime.seconds() < 1){
+            drive(.1, 0, 0);
+            telemetry.addData("Status", "Autonomous Running");
+            telemetry.update();
+        } else{
+            telemetry.addData("Status", "Autonomous Finished");
+            telemetry.update();
+            drive(0,0,0);
         }
-        telemetry.addData("Status", "Autonomous Running");
-        telemetry.update();
-    }
-
-    @Override
-    public void stop() {
-        telemetry.addData("Status", "Autonomous Finished");
-        telemetry.update();
-        //mecanumHardware.stop();
-
 
     }
 
+    public void drive(double forward, double right, double rotate) {
+        // This calculates the power needed for each wheel based on the amount of forward,
+        // strafe right, and rotate
+        double frontLeftPower = forward + right + rotate;
+        double frontRightPower = forward - right - rotate;
+        double backRightPower = forward + right - rotate;
+        double backLeftPower = forward - right + rotate;
+
+        double maxPower = 1.0;
+        double maxSpeed = 1.0;  // make this slower for outreaches
+
+        // This is needed to make sure we don't pass > 1.0 to any wheel
+        // It allows us to keep all of the motors in proportion to what they should
+        // be and not get clipped
+        maxPower = Math.max(maxPower, Math.abs(frontLeftPower));
+        maxPower = Math.max(maxPower, Math.abs(frontRightPower));
+        maxPower = Math.max(maxPower, Math.abs(backRightPower));
+        maxPower = Math.max(maxPower, Math.abs(backLeftPower));
+
+        // We multiply by maxSpeed so that it can be set lower for outreaches
+        // When a young child is driving the robot, we may not want to allow full
+        // speed.
+        frontLeftDrive.setPower(maxSpeed * (frontLeftPower / maxPower));
+        frontRightDrive.setPower(maxSpeed * (frontRightPower / maxPower));
+        backLeftDrive.setPower(maxSpeed * (backLeftPower / maxPower));
+        backRightDrive.setPower(maxSpeed * (backRightPower / maxPower));
+    }
 }
